cb6e3f611d0a5e08157885f85313de7f5eb0a5be
Arian Tershnjaku <arian.t@gjirafa.com>
2024-02-25 23:55:29 +0100

    Loading question from fake API

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,21 @@
-import React from "react";
+import React, { useEffect } from "react";
 import DateCounter from "./DateCounter";
 import Header from "./Header";
 import Main from "./Main";
 
 function App() {
+  useEffect(() => {
+    fetch("http://localhost:3001/questions").then((response) => {
+      response
+        .json()
+        .then((data) => {
+          console.log(data);
+        })
+        .catch((error) => {
+          console.error(error);
+        });
+    });
+  }, []);
   return (
     <div className="app">
       <Header />
